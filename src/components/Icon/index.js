2fd9d85e1579972ba38bf0a3ca44b0fd421373ed
[v_shilili] <v_shilili@tencent.com>
2020-07-24 20:49:54 +0800

    feat:icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -13,7 +13,7 @@ function Icon({ icon: IconComponent, width = 24, height = 24, color, opacity, ..
 }
 
 Icon.propTypes = {
-  icon: PropTypes.element,
+  icon: PropTypes.elementType,
   width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   color: PropTypes.string,
